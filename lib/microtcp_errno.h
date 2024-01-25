8ea160f09f639d7fb3eec945b9cc10ca1d3620cc
IOANNIS SPYROPOULOS <csd5072@csd.uoc.gr>
2024-01-25 21:32:04 +0200

    Added all files except build files

---
+++ lib/microtcp_errno.h
@@ -0,0 +1,89 @@
+#ifndef MICROTCP_ERRNO_H
+#define MICROTCP_ERRNO_H
+
+#include <stdio.h>
+
+#define DEBUG
+
+enum MICROTCP_ERRNO
+{
+    ALL_GOOD = 0,
+
+    ERROR,
+
+    /* SPECIFICS */
+    NULL_POINTER_ARGUMENT,
+    MALLOC_FAILED,
+    SOCKET_STATE_NOT_READY,
+    TIMEOUT_SET_FAILED,
+    INVALID_IP_VERSION,
+    BITSTREAM_CREATION_FAILED,
+    BITSTREAM_EXTRACTION_FAILED,
+    SYN_PACKET_EXPECTED,
+    ACK_SYN_PACKET_EXPECTED,
+    ACK_PACKET_EXPECTED,
+    ACK_NUMBER_MISMATCH,
+    HANDSHAKE_FAILED,
+    SENDTO_FAILED,
+    RECVFROM_CORRUPTED
+};
+
+enum MICROTCP_ERRNO MICRO_ERRNO = ALL_GOOD;
+
+static void microtcp_set_errno(enum MICROTCP_ERRNO errno_, const char *function_name_, int line_)
+{
+    const char *error_message;
+    MICRO_ERRNO = errno_;
+
+    switch (errno_)
+    {
+    case NULL_POINTER_ARGUMENT:
+        "NULL pointer was given as argument.";
+        break;
+    case MALLOC_FAILED:
+        error_message = "Memory allocation failed.";
+        break;
+    case SOCKET_STATE_NOT_READY:
+        error_message = "Socket state is not in ready state.";
+        break;
+    case TIMEOUT_SET_FAILED:
+        error_message = "Setting timeout in recvfrom() failed.";
+        break;
+    case BITSTREAM_CREATION_FAILED:
+        error_message = "Bit-stream creation failed.";
+        break;
+    case BITSTREAM_EXTRACTION_FAILED:
+        error_message = "Bit-stream extraction failed.";
+        break;
+    case SYN_PACKET_EXPECTED:
+        error_message = "Expected packet with SYN flag.";
+        break;
+    case ACK_SYN_PACKET_EXPECTED:
+        error_message = "Expected packet with ACK and SYN flags.";
+        break;
+    case ACK_PACKET_EXPECTED:
+        error_message = "Expected packet with ACK flag.";
+        break;
+    case ACK_NUMBER_MISMATCH:
+        error_message = "ACK number does not match the expected value.";
+        break;
+    case HANDSHAKE_FAILED:
+        error_message = "Three-way handshake failed between server and client.";
+        break;
+    case SENDTO_FAILED:
+        error_message = "Sendind bit-stream with UDP::sendto() failed.";
+        break;
+    case RECVFROM_CORRUPTED:
+        error_message = "UDP::recvfrom returned corrupted data.";
+        break;
+    default:
+        error_message = "Unknown microtcp error number (default).";
+        break;
+    }
+
+#ifdef DEBUG
+    fprintf(stderr, "Error in line %d (%s): %s\n", line_, function_name_, error_message);
+#endif
+}
+
+#endif
---
+++ test/test_microtcp_server.c
@@ -0,0 +1,87 @@
+/*
+ * microtcp, a lightweight implementation of TCP for teaching,
+ * and academic purposes.
+ *
+ * Copyright (C) 2015-2017  Manolis Surligas <surligas@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * You can use this file to write a test microTCP server.
+ * This file is already inserted at the build system.
+ */
+
+/**
+ * CS335 - Project Phase A
+ * 
+ * Ioannis Spyropoulos - csd5072
+ * Georgios Evangelinos - csd4624
+ * Niki Psoma - csd5038
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "../lib/microtcp.h"
+
+#define PORT 54321
+
+int
+main(int argc, char **argv)
+{
+    struct sockaddr_in servaddr;
+    struct sockaddr_in clientaddr;
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&clientaddr, 0, sizeof(clientaddr));
+
+    microtcp_sock_t tcpsocket = microtcp_socket(AF_INET, SOCK_DGRAM, 0);
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    printf("Binding...\n");
+    microtcp_bind(&tcpsocket, (const struct sockaddr*) &servaddr, sizeof(servaddr));
+    printf("Bind successful\n");
+
+    printf("Waiting for connection request...\n");
+    microtcp_accept(&tcpsocket, (struct sockaddr*) &clientaddr, sizeof(clientaddr));
+    printf("Connected\n");
+
+    const char* rmsg = "Message received";
+
+    char buff[1024] = {0};
+    do
+    {
+        int bytes_rec = microtcp_recv(&tcpsocket, buff, 1024, NO_FLAGS_BITS);
+        buff[bytes_rec] = '\0';
+        if (tcpsocket.state == ESTABLISHED)
+        {
+            printf("From client: %s", buff);
+            microtcp_send(&tcpsocket, rmsg, 17, NO_FLAGS_BITS);
+        }
+    } while (tcpsocket.cliaddr != NULL);
+
+    printf("Connection closed by peer\n");
+
+    return EXIT_SUCCESS;
+}
